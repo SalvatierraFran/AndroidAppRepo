72bbd4f09c4e89ba70e18b1a9a383305312b703d
SOFTTEK\franco.salvatierra <asdasd>
2018-02-20 17:06:04 -0300

    Realizado el Home y Map.

---
+++ app/src/main/java/com/example/francosalvatierra/androidapp/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.francosalvatierra.androidapp;
+
+//import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Fragment currentFrag = null;
+    FragmentTransaction ft = null;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+
+                    currentFrag = new HomeFragment();
+                    ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.miContainer, currentFrag);
+                    ft.commit();
+                    return true;
+
+                case R.id.navigation_map:
+
+                    currentFrag = new MapFragment();
+                    ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.miContainer, currentFrag);
+                    ft.commit();
+                    return true;
+            }
+
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ft = getSupportFragmentManager().beginTransaction();
+        currentFrag = new HomeFragment();
+        ft.replace(R.id.miContainer, currentFrag);
+        ft.commit();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
